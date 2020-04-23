1e0c0485d1e65ce7aeef12cc05f90821c578d989
Nickswagg <tngongangnicolas@gmail.com>
2020-04-23 16:29:53 -0400

    Translating my weather app in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "zipcodes";
+import $ from "jquery";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4 offset-md-4 box-middle">
+          <h2 className="title">
+            Weather App&nbsp;&nbsp;<i className="fas fa-cloud"></i>
+          </h2>
+          <label>Type in Your ZIPCODE to get weather :</label>
+          <input type="text" className="form-control" id="zipinput"></input>
+          <button
+            type="button" onclick= "getzip()"
+            id="zipweather"
+            className="btn btn-primary btn-lg"
+          >
+            GET WEATHER
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
